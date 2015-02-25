41eeb0aff391d925e2bec440140382d6eccda16d
zhaominhe <zhaominhe@gmail.com>
2015-02-25 18:12:10 +0800

    some table added

--- p2p-core/core-service/src/sql/member-schema.sql
+++ p2p-core/core-service/src/sql/member-schema.sql
@@ -6,36 +6,138 @@ drop table if exists member_card_info;
 drop table if exists member_job_info;
 
 CREATE TABLE member (
-  id BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '会员ID标识',
+  id BIGINT NOT NULL AUTO_INCREMENT COMMENT '会员标识ID',
   email VARCHAR(50) DEFAULT NULL COMMENT '邮箱',
   uname VARCHAR(50) DEFAULT NULL COMMENT '用户名',
   pwd VARCHAR(60) DEFAULT NULL COMMENT '密码',
   -- trade_pwd VARCHAR(60) DEFAULT NULL COMMENT '交易密码',
   mobile_phone VARCHAR(15) DEFAULT NULL COMMENT '手机号码',
-  purpose INT(11) DEFAULT '0' COMMENT '意向；1：投资；2：贷款',
-  source INT(4) DEFAULT NULL COMMENT '客户来源',
-  province INT(11) DEFAULT '0' COMMENT '省份或者直辖市',
-  city INT(11) DEFAULT '0' COMMENT '城市',
-  is_agree INT(4) DEFAULT '0' COMMENT '是否同意（条款）',
-  auth_id BIGINT(20) DEFAULT '0' COMMENT '实名认证ID',
-  is_locked CHAR(1) DEFAULT '0' COMMENT '是否锁定,  1:是； 0：否',
-  is_withhold CHAR(1) DEFAULT '0' COMMENT '是否代扣,  1:是； 0：否',
-  created_time DATETIME DEFAULT now() COMMENT '创建时间',
-  auth_status INT(4) DEFAULT '0' COMMENT '会员认证状态',
-  PRIMARY KEY (id),
-  UNIQUE KEY idx_mobile_phone (mobile_phone) USING BTREE,
-  UNIQUE KEY idx_uname (uname) USING BTREE
-) ENGINE=INNODB DEFAULT CHARSET=utf8 COMMENT='会员注册表信息'
+  purpose BIGINT DEFAULT 1 COMMENT '意向；参见sys_dict中type=member_purpose,1001:投资，1002:贷款',
+  source BIGINT DEFAULT NULL COMMENT '客户来源；参见sys_dict中type=member_source,1201：web，1202：app，1203：wechat，1204：其它',
+  province BIGINT DEFAULT 0 COMMENT '省份或者直辖市',
+  city BIGINT DEFAULT 0 COMMENT '城市',
+  is_agree bool default true  comment '是否同意（条款）',
+  auth_id BIGINT DEFAULT 0 COMMENT '实名认证ID',
+  locked bool default false COMMENT '是否锁定',
+  is_withhold bool default true COMMENT '是否代扣',
+  auth_state BIGINT DEFAULT 0 COMMENT '会员认证状态，参见sys_dict中type=member_auth_state，1301:,1302:,',
+  rank BIGINT DEFAULT 0 comment '会员级别，参见sys_dict中type=member_rank，1401:1402:1403 ',
+  created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  constraint pk_member primary key(id),
+  UNIQUE KEY idx_member_mobile_phone (mobile_phone) USING BTREE,
+  UNIQUE KEY idx_member_uname (uname) USING BTREE
+) ENGINE=INNODB CHARSET=utf8 COMMENT='会员注册表信息';
 
 CREATE TABLE member_account (
-  id BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '标识ID',
-  m_id BIGINT(20) DEFAULT NULL COMMENT '会员ID标识',
-  usable_amount DECIMAL(18,2) DEFAULT '0.00' COMMENT '可用金额',
-  frozen_amount DECIMAL(18,2) DEFAULT '0.00' COMMENT '冻结金额',
-  earned_amount DECIMAL(18,2) DEFAULT '0.00' COMMENT '赚的金额(利息)',
-  ext_amount DECIMAL(18,2) DEFAULT '0.00' COMMENT '扩展金额.如：做活动',
-  bonus_amount DECIMAL(18,2) DEFAULT '0.00' COMMENT '用户得到的奖金',
-  PRIMARY KEY (id),
-  UNIQUE KEY idx_uq_mid (m_id) USING BTREE,
-  CONSTRAINT idx_MA_mid FOREIGN KEY (m_id) REFERENCES member (id)
-) ENGINE=INNODB DEFAULT CHARSET=utf8 COMMENT='用户账户信息表'
+  id BIGINT NOT NULL AUTO_INCREMENT COMMENT '标识ID',
+  m_id BIGINT COMMENT '会员标识ID',
+  uname varchar(50) comment '用户名',
+  ma_type bigint comment '账户类型，参见sys_dict中type=ma_type，2001：可用金额，2002：冻结，2003：利息，2004：活动金，2005：奖金，2006：积分',
+  amount DECIMAL(18,2) DEFAULT 0.00 COMMENT '可用金额',
+  available bool default true comment '可用否',
+  created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  constraint pk_member_account primary key(id),
+  CONSTRAINT idx_ma_mid FOREIGN KEY (m_id) REFERENCES member (id)
+) ENGINE=INNODB DEFAULT CHARSET=utf8 COMMENT='用户账户信息表';
+create index idx_ma_m_id on member_account(m_id);
+create index idx_ma_uname on member_account(uname);
+
+create table member_account_log (
+  id bigint auto_increment comment '标识ID',
+  m_id bigint comment '会员标识ID',
+  uname varchar(50) comment '用户名',
+  ma_id bigint comment '会员账户id标识',
+  log_type bigint comment '账户变动类型',
+  log_amount DECIMAL(18,2) DEFAULT 0.00 comment '发生额',
+  balance DECIMAL(18,2) DEFAULT 0.00 comment '余额',
+  created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  created_uid bigint comment '创建者id',
+  constraint pk_member_account_log primary key(id)
+)ENGINE=INNODB DEFAULT CHARSET=utf8 COMMENT='用户账户流水表';
+create index idx_mal_m_id on member_account_log(m_id);
+create index idx_mal_ma_id on member_account_log(ma_id);
+create index idx_mal_uname on member_account_log(uname);
+
+CREATE TABLE member_card_info (
+  id BIGINT NOT NULL AUTO_INCREMENT COMMENT '标识ID',
+  m_id BIGINT DEFAULT NULL COMMENT '会员ID标识',
+  uname varchar(50) comment '用户名',
+  account_name VARCHAR(20) DEFAULT NULL COMMENT '开户人',
+  bank VARCHAR(50) DEFAULT NULL COMMENT '银行',
+  bank_code BIGINT DEFAULT NULL COMMENT '银行代码，联行号',
+  bank_province VARCHAR(30) DEFAULT NULL COMMENT '银行所在省份',
+  bank_city VARCHAR(30) DEFAULT NULL COMMENT '银行所在城市',
+  bank_address VARCHAR(100) DEFAULT NULL COMMENT '开户行的地址',
+  bank_name VARCHAR(80) DEFAULT NULL COMMENT '开户行',
+  bank_card VARCHAR(40) DEFAULT NULL,
+  bind_status bool default true comment '是否绑定状态',
+  is_withhold bool default false comment '是否是代扣卡',
+  is_valid bool default true comment '是否有效',
+  created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  constraint pk_member_card_info primary key(id)
+) ENGINE=INNODB CHARSET=utf8 COMMENT='用户银行卡信息表';
+create index idx_mci_m_id on member_card_info(m_id);
+create index idx_mci_uname on member_card_info(uname);
+
+create table member_security (
+  id bigint auto_increment comment '标识ID',
+  m_id bigint comment '会员标识ID',
+  uname varchar(50) comment '用户名',
+  auth_type bigint comment '认证类型，参考sys_dict中type=auth_type',
+  auth_name varchar(100) comment '认证类型名称',
+  auth_desc varchar(200) comment '认证类型描述',
+  auth_result varchar(20) comment '认证结果',
+  prop_name varchar(100) comment '认证属性',
+  prop_value varchar(200) comment '认证属性值',
+  created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  constraint pk_member_security primary key(id)
+) charset=utf8 engine=InnoDB;
+create index idx_ms_m_id on member_security(m_id);
+create index idx_ms_uname on member_security(uname);
+
+create table member_identification (
+  id bigint auto_increment comment '标识ID',
+  m_id bigint comment '会员标识ID',
+  uname varchar(50) comment '用户名',
+  iden_type bigint comment '识别类型，参考sys_dict中type=iden_type',    -- idcard,baseinfo,contactinfo,job,income,credit_report,live,house,car,marriage,edu,tech,mobile,weibo,website,wechat,mortgage,warrant
+  iden_name varchar(100) comment '识别名称',
+  prop_name varchar(200) comment '属性名称',
+  prop_value varchar(200) comment '属性值',
+  resource_id bigint comment '资源id',
+  created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  constraint pk_member_iden primary key(id)
+) charset=utf8 engine=InnoDB;
+create index idx_mi_m_id on member_identification(m_id);
+create index idx_mi_uname on member_identification(uname);
+
+CREATE TABLE member_realname_auth (
+  id BIGINT NOT NULL AUTO_INCREMENT COMMENT '标识ID',
+  real_name VARCHAR(20) DEFAULT NULL COMMENT '姓名',
+  id_card VARCHAR(20) DEFAULT NULL COMMENT '身份证号',
+  remark varchar(200),DEFAULT null comment '备注',
+  pic_path VARCHAR(100) DEFAULT NULL COMMENT '身份证图片路径',
+  pic_suffix VARCHAR(10) DEFAULT NULL COMMENT '图片的后缀',
+  pic_binary TEXT COMMENT '图片二进制内容',
+  is_authed bool default false comment '是否认证成功',
+  created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  constraint pk_member_realname_auth primary key(id)
+) ENGINE=INNODB CHARSET=utf8 COMMENT='用户实名认证数据库信息表';
+create index idx_mra_id_card on member_realname_auth(id_card);
+
+/****
+create table sys_biz (
+  id bigint auto_incrementcomment '隶属组织id',
+  code varchar(20) comment '隶属组织id',
+  biz_name varchar(200) comment '隶属组织id',
+  biz_addr varchar(200) comment '隶属组织id',
+  biz_legal_person varchar(100) comment '隶属组织id',
+  contact varchar(100) comment '隶属组织id',
+  tel varchar(20) comment '隶属组织id',
+  email varchar(50) comment '隶属组织id',
+  user_id bigintcomment '隶属组织id',
+  created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP  comment '创建时间',
+  constraint pk_sys_biz primary key(id)
+) charset=utf8 ENGINE=InnoDB;
+create index idx_sys_biz_uid on sys_biz(user_id);
+
+*************/
--- p2p-core/core-service/src/sql/user-schema.sql
+++ p2p-core/core-service/src/sql/user-schema.sql
@@ -7,179 +7,119 @@ drop table if exists sys_role;
 drop table if exists sys_area;
 drop table if exists sys_bank_info;
 
-/********--
-create table sessions (
-  id varchar(200),
-  session varchar(2000),
-  constraint pk_sessions primary key(id)
-) charset=utf8 ENGINE=InnoDB;
---
-****/
 create table sys_user (
-  id bigint auto_increment  comment "id",
-  org_id bigint  comment "隶属组织id",
-  user_name varchar(100)  comment "用户名",
-  email varchar(100)  comment "email",
-  mobile varchar(15)  comment "手机号",
-  password varchar(100)  comment "密码",
-  salt varchar(100)  comment "盐",
-  -- invest_state int  comment "",
-  -- loan_state int  comment "隶属组织id",
-  -- credit_rate int  comment "隶属组织id",
-  locked bool default false  comment "是否锁定",
-  created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP  comment "创建时间",
+  id bigint auto_increment  comment 'id',
+  org_id bigint  comment '隶属组织id',
+  uname varchar(50)  comment '用户名',
+  email varchar(50)  comment 'email',
+  mobile varchar(15)  comment '手机号',
+  pwd varchar(60)  comment '密码',
+  salt varchar(100)  comment '盐',
+  locked bool default false  comment '是否锁定',
+  created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP  comment '创建时间',
   constraint pk_sys_user primary key(id)
 ) charset=utf8 ENGINE=InnoDB  comment='管理人员表';
 create unique index idx_su_username on sys_user(username);
 create index idx_su_org_id on sys_user(org_id);
 
-/*******
-create table sys_user_security (
-  id bigint auto_incrementcomment "隶属组织id",
-  user_id bigintcomment "隶属组织id",
-  auth_type varchar(10) comment "隶属组织id",
-  auth_name varchar(100) comment "隶属组织id",
-  auth_desc varchar(200) comment "隶属组织id",
-  auth_result varchar(20) comment "隶属组织id",
-  prop_name varchar(100) comment "隶属组织id",
-  prop_value varchar(200) comment "隶属组织id",
-  created_time datetimecomment "隶属组织id",
-  last_update_time datetimecomment "隶属组织id",
-  constraint pk_sys_user_security primary key(id)
-) charset=utf8 engine=InnoDB;
-create index idx_sus_user_id on sys_user_security(user_id);
 
-create table sys_user_identification (
-  id bigint auto_incrementcomment "隶属组织id",
-  user_id bigintcomment "隶属组织id",
-  iden_type intcomment "隶属组织id",    //idcardcomment "隶属组织id",baseinfocomment "隶属组织id",contactinfocomment "隶属组织id",jobcomment "隶属组织id",incomecomment "隶属组织id",credit_reportcomment "隶属组织id",livecomment "隶属组织id",housecomment "隶属组织id",carcomment "隶属组织id",marriagecomment "隶属组织id",educomment "隶属组织id",techcomment "隶属组织id",mobilecomment "隶属组织id",weibocomment "隶属组织id",websitecomment "隶属组织id",wechatcomment "隶属组织id",mortgagecomment "隶属组织id",warrant
-  iden_name varchar(100) comment "隶属组织id",
-  prop_name varchar(200) comment "隶属组织id",
-  prop_value varchar(200) comment "隶属组织id",
-  resource_id bigintcomment "隶属组织id",
-  created_time datetimecomment "隶属组织id",
-  constraint pk_sui primary key(id)
-) charset=utf8 engine=InnoDB;
-create index idx_sui_user_id on sys_user_identification(user_id);
-*******************/
 create table sys_app (
-  id bigint auto_increment comment "id",
-  app_name varchar(100) comment "应用名称",
-  app_key varchar(100) comment "应用key",
-  app_secret varchar(100) comment "应用secret",
-  available bool default false comment "可用否",
-  created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP  comment "创建时间",
+  id bigint auto_increment comment 'id',
+  app_name varchar(100) comment '应用名称',
+  app_key varchar(100) comment '应用key',
+  app_secret varchar(100) comment '应用secret',
+  available bool default true comment '可用否',
+  created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP  comment '创建时间',
   constraint pk_sys_app primary key(id)
 ) charset=utf8 ENGINE=InnoDB  comment='平台应用管理表';
 create unique index idx_sys_app_app_key on sys_app(app_key);
 
 create table sys_user_app_roles (
-  id bigint auto_increment comment "id",
-  user_id bigint comment "用户id",
-  app_id bigint comment "应用id",
-  role_ids varchar(100) comment "角色id集",
-  created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP  comment "创建时间",
+  id bigint auto_increment comment 'id',
+  user_id bigint comment '用户id',
+  app_id bigint comment '应用id',
+  role_ids varchar(100) comment '角色id集',
+  created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP  comment '创建时间',
   constraint pk_sys_user_app_roles primary key(id)
 ) charset=utf8 ENGINE=InnoDB comment='用户应用角色关系表';
 create unique index idx_sys_UAR_ids on sys_user_app_roles(user_id, app_id);
 
-
 create table sys_org (
-  id bigint auto_increment comment "隶属组织id",
-  org_name varchar(100) comment "组织名",
-  parent_id bigint comment "上级id",
-  parent_ids varchar(100) comment "上级id集",
-  available bool default false comment "可用否",
-  created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP  comment "创建时间",
+  id bigint auto_increment comment '隶属组织id',
+  org_name varchar(100) comment '组织名',
+  parent_id bigint comment '上级id',
+  parent_ids varchar(100) comment '上级id集',
+  available bool default true comment '可用否',
+  created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP  comment '创建时间',
   constraint pk_sys_organization primary key(id)
 ) charset=utf8 ENGINE=InnoDB  comment='管理组织信息表';
 create index idx_sys_organization_parent_id on sys_organization(parent_id);
 create index idx_sys_organization_parent_ids on sys_organization(parent_ids);
 
-
 create table sys_resource (
-  id bigint auto_increment comment "id",
-  res_name varchar(100) comment "资源名称",
-  res_type varchar(50) comment "资源类型",
-  url varchar(200) comment "资源链接",
-  parent_id bigint comment "上级id",
-  parent_ids varchar(100) comment "上级id集合",
-  permission varchar(100) comment "所需权限",
-  available bool default false comment "可用否",
-  created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP  comment "创建时间",
+  id bigint auto_increment comment 'id',
+  res_name varchar(100) comment '资源名称',
+  res_type bigint comment '资源类型,参见sys_dict中type=res_type,9001:link,9002:static image,9003:static html,9009:others',
+  url varchar(200) comment '资源链接',
+  parent_id bigint comment '上级id',
+  parent_ids varchar(100) comment '上级id集合',
+  permission varchar(100) comment '所需权限',
+  available bool default true comment '可用否',
+  created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP  comment '创建时间',
   constraint pk_sys_resource primary key(id)
 ) charset=utf8 ENGINE=InnoDB  comment='资源表';
 create index idx_sys_resource_parent_id on sys_resource(parent_id);
 create index idx_sys_resource_parent_ids on sys_resource(parent_ids);
 
 create table sys_role (
-  id bigint auto_increment comment "id",
-  role varchar(100) comment "角色名",
-  description varchar(100) comment "角色描述",
-  resource_ids varchar(100) comment "资源集",
-  available bool default false comment "可用否",
-  created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP  comment "创建时间",
+  id bigint auto_increment comment 'id',
+  role varchar(100) comment '角色名',
+  description varchar(100) comment '角色描述',
+  resource_ids varchar(100) comment '资源集',
+  available bool default true comment '可用否',
+  created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP  comment '创建时间',
   constraint pk_sys_role primary key(id)
 ) charset=utf8 ENGINE=InnoDB  comment='角色表';
 create index idx_sys_role_resource_ids on sys_role(resource_ids);
 
-/***
-create table sys_area (
-  id bigint auto_increment comment "id",
-  parent_id bigint comment "上级id",
-  parent_ids varchar(100) comment "上级id集",
-  area_code char(6) comment "编码",
-  area_name varchar(100) comment "名称",
-  available bool default false comment "可用否",
-  created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP  comment "创建时间",
-  constraint pk_sys_area primary key(id)
-) charset=utf8 ENGINE=InnoDB  comment='地区字典表';
-create index idx_sys_area_parent_id on sys_area(parent_id);
-***/
 
 CREATE TABLE sys_area (
-  id BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '编号',
-  parent_id BIGINT(20) NOT NULL COMMENT '父级编号',
-  parent_ids VARCHAR(255) COLLATE utf8_bin NOT NULL COMMENT '所有父级编号',
-  area_code VARCHAR(100) COLLATE utf8_bin DEFAULT NULL COMMENT '区域编码',
-  area_name VARCHAR(100) COLLATE utf8_bin NOT NULL COMMENT '区域名称',
-  type CHAR(1) COLLATE utf8_bin DEFAULT NULL COMMENT '区域类型（1：国家；2：省份、直辖市；3：地市；4：区县）',
-  create_date DATETIME DEFAULT NULL COMMENT '创建时间',
-  remarks VARCHAR(255) COLLATE utf8_bin DEFAULT NULL COMMENT '备注信息',
-  del_flag CHAR(1) COLLATE utf8_bin NOT NULL DEFAULT '0' COMMENT '删除标记（0：正常；1：删除）',
+  id BIGINT NOT NULL AUTO_INCREMENT COMMENT '编号',
+  parent_id BIGINT NOT NULL COMMENT '父级编号',
+  parent_ids VARCHAR(255)  NOT NULL COMMENT '所有父级编号',
+  area_code VARCHAR(100)  DEFAULT NULL COMMENT '区域编码',
+  area_name VARCHAR(100)  NOT NULL COMMENT '区域名称',
+  area_type BIGINT  DEFAULT 0 COMMENT '区域类型,见sys_dict中type=area_type（8001：国家；8002：省份、直辖市；8003：地市；8004：区县）',
+  remarks VARCHAR(255)  DEFAULT NULL COMMENT '备注信息',
+  available bool default true COMMENT '可用否',
+  created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY (id),
   KEY sys_area_parent_id (parent_id) USING BTREE,
   KEY sys_area_parent_ids (parent_ids) USING BTREE,
   KEY sys_area_del_flag (del_flag) USING BTREE
-) ENGINE=INNODB AUTO_INCREMENT=494 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='区域表'
+) ENGINE=INNODB COMMENT='区域表';
 
 create table sys_bank_info (
-  id BIGINT AUTO_INCREMENT COMMENT "id",
-  bank_name VARCHAR(100) COMMENT "银行名称",
-  bank_code BIGINT COMMENT "银行代码",
-  branch_name VARCHAR(100) COMMENT "分支名称",
-  STATUS INT COMMENT "状态",
-  province_name VARCHAR(30) COMMENT "省",
-  city_name VARCHAR(30) COMMENT "市",
-  created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP  COMMENT "创建时间",
+  id BIGINT AUTO_INCREMENT COMMENT 'id',
+  bank_name VARCHAR(100) COMMENT '银行名称',
+  bank_code BIGINT COMMENT '银行代码',
+  branch_name VARCHAR(100) COMMENT '分支名称',
+  available bool default true comment '可用否',
+  province_name VARCHAR(30) COMMENT '省',
+  city_name VARCHAR(30) COMMENT '市',
+  created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP  COMMENT '创建时间',
   CONSTRAINT pk_sys_bank_info PRIMARY KEY(id)
 ) CHARSET=utf8 ENGINE=INNODB  COMMENT='银行字典表';
 create index idx_sys_bank_info_code ON sys_bank_info(bank_code);
 
-/****
-create table sys_biz (
-  id bigint auto_incrementcomment "隶属组织id",
-  code varchar(20) comment "隶属组织id",
-  biz_name varchar(200) comment "隶属组织id",
-  biz_addr varchar(200) comment "隶属组织id",
-  biz_legal_person varchar(100) comment "隶属组织id",
-  contact varchar(100) comment "隶属组织id",
-  tel varchar(20) comment "隶属组织id",
-  email varchar(50) comment "隶属组织id",
-  user_id bigintcomment "隶属组织id",
-  created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP  comment "创建时间",
-  constraint pk_sys_biz primary key(id)
-) charset=utf8 ENGINE=InnoDB;
-create index idx_sys_biz_uid on sys_biz(user_id);
-
-*************/
+create table sys_dict (
+  id bigint auto_increment comment 'id',
+  label varchar(50) comment '标签名',
+  value bigint comment '值',
+  type varchar(20) comment '类型',
+  description varchar(50) comment '描述',
+  sort bigint comment '排序值',
+  available bool default true comment '可用否',
+  CONSTRAINT pk_sys_dict PRIMARY KEY(id)
+) CHARSET=utf8 ENGINE=INNODB  COMMENT='系统字典表';
+create index idx_sys_dict_value ON sys_dict(value);
